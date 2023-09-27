e3c50ba4f20b43e3f820b0bea8a6bc94a6ad9e08
Mohamed TCHOKETCH <mohamedtchoketch0@gmail.com>
2023-09-27 05:45:40 +0200

    Completed assignment

--- src/components/ColorList.js
+++ src/components/ColorList.js
@@ -9,18 +9,29 @@ function ColorList() {
     "hotpink",
   ];
 
+  const colorElements = colors.map((color) => {
+    return (
+      <li key={color} style={{ color: color }}>
+        {color}
+      </li>
+    );
+  });
   return (
     <div>
       <h1>Top 5 CSS Colors</h1>
       <ol>
-        <li style={{ color: colors[0] }}>{colors[0]}</li>
-        <li style={{ color: colors[1] }}>{colors[1]}</li>
-        <li style={{ color: colors[2] }}>{colors[2]}</li>
-        <li style={{ color: colors[3] }}>{colors[3]}</li>
-        <li style={{ color: colors[4] }}>{colors[4]}</li>
+        {/* display the array of <li> elements here! */}
+        {colorElements}
       </ol>
     </div>
   );
 }
+
+// const users = [
+//   { id: 1, firstName: "Duane", lastName: "Watson" },
+//   { id: 2, firstName: "Duane", lastName: "Johnson" },
+// ];
+
+// const userHeading = users.map((user) => {return <h1 key={user.id}>{user.firstName}</h1>});
 
 export default ColorList;
